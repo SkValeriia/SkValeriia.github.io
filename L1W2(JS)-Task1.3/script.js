3944dc60aad47c46bac947de338df170ec4cbd61
Valeriia Skutelnyk <42683748+SkutelnykValeriia@users.noreply.github.com>
2018-11-24 13:32:30 +0300

    Add files via upload

--- L1W2(JS)-Task1.3/script.js
+++ L1W2(JS)-Task1.3/script.js
@@ -1,9 +1,9 @@
 let arr = [1, 4, 2];
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
-console.log(arr.reduce(reducer));
 
 let sumNumbers = arr.reduce(reducer);
 let averageNumber = sumNumbers/arr.length;
 
+console.log("Average number is: " + averageNumber);
 alert ("Average number is: " + averageNumber);
